b4e2d2174c13abf13e2fa21db46ae75369c861f5
akhisyabab <akhisyabab@monetizemore.com>
2019-03-09 21:35:39 +0700

    inventory.sql

--- inventory.sql
+++ inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 09, 2019 at 03:06 PM
+-- Generation Time: Mar 09, 2019 at 03:35 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -32,7 +32,6 @@ CREATE TABLE `barang` (
   `id_barang` int(11) NOT NULL,
   `kode_barang` varchar(10) DEFAULT NULL,
   `nama_barang` varchar(50) DEFAULT NULL,
-  `harga` int(11) DEFAULT NULL,
   `stok` int(5) DEFAULT NULL,
   `foto_barang` varchar(100) DEFAULT NULL,
   `id_jenis` int(11) DEFAULT NULL,
@@ -44,11 +43,9 @@ CREATE TABLE `barang` (
 -- Dumping data for table `barang`
 --
 
-INSERT INTO `barang` (`id_barang`, `kode_barang`, `nama_barang`, `harga`, `stok`, `foto_barang`, `id_jenis`, `id_merk`, `kode_supplier`) VALUES
-(2, 'BRG0002', 'Mesin', 3000000, 2, 'barang_1526268105.jpg', 1, 1, 'sp002'),
-(3, 'BRG0003', 'Knalpot', 500000, 4, 'barang_1526268446.jpg', 1, 1, 'sp002'),
-(4, 'BRG0004', 'Lampu Sen', 20000, 3, 'barang_1526268494.jpg', 1, 1, 'sp002'),
-(5, 'BRG0005', 'Oli Mesin', 50000, 20, 'barang_1526268525.png', 1, 1, 'sp001');
+INSERT INTO `barang` (`id_barang`, `kode_barang`, `nama_barang`, `stok`, `foto_barang`, `id_jenis`, `id_merk`, `kode_supplier`) VALUES
+(6, '1234', 'hp', 3, 'barang_1551363990.PNG', 2, 1, NULL),
+(7, 'BRG0007', 'jjk', 4, 'barang_1551623482.png', 2, 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -60,18 +57,22 @@ CREATE TABLE `barang_keluar` (
   `id_barang_keluar` int(11) NOT NULL,
   `kode_barang` varchar(20) DEFAULT NULL,
   `tgl_keluar` date DEFAULT NULL,
-  `jumlah_jual` int(11) DEFAULT NULL
+  `jumlah_jual` int(11) DEFAULT NULL,
+  `harga_jual` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `barang_keluar`
 --
 
-INSERT INTO `barang_keluar` (`id_barang_keluar`, `kode_barang`, `tgl_keluar`, `jumlah_jual`) VALUES
-(1, 'BRG0003', '2018-12-31', 7),
-(2, 'BRG0002', '2018-12-31', 5),
-(3, 'BRG0002', '2019-03-05', 2),
-(4, 'BRG0003', '2019-03-13', 2);
+INSERT INTO `barang_keluar` (`id_barang_keluar`, `kode_barang`, `tgl_keluar`, `jumlah_jual`, `harga_jual`) VALUES
+(1, 'BRG0003', '2018-12-31', 7, 0),
+(2, 'BRG0002', '2018-12-31', 5, 0),
+(4, 'BRG0005', '2019-02-27', 5, 0),
+(5, 'BRG0005', '2019-02-27', 3, 0),
+(6, 'BRG0005', '2019-02-28', 5, 0),
+(7, 'BRG0002', '2019-03-04', 4, 0),
+(8, 'BRG0007', '2018-12-01', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -92,10 +93,10 @@ CREATE TABLE `barang_masuk` (
 --
 
 INSERT INTO `barang_masuk` (`id_barang_masuk`, `kode_barang`, `kode_supplier`, `jumlah_beli`, `harga_beli`) VALUES
-(1, 'BRG0002', 'sp001', 2, 40000000),
-(2, 'BRG0003', 'sp002', 2, 3000000),
-(3, 'BRG0004', 'sp002', 2, 2000000),
-(4, 'BRG0005', 'sp002', 1, 2000000);
+(1, 'BRG0002', 'sp001', 1, 10000000),
+(2, 'BRG0002', 'sp001', 1, 12000000),
+(3, 'BRG0002', 'sp001', 10, 23000),
+(4, 'BRG0007', 'supplierb', 2, 2000000);
 
 -- --------------------------------------------------------
 
@@ -134,8 +135,36 @@ CREATE TABLE `jenis_barang` (
 
 INSERT INTO `jenis_barang` (`id_jenis`, `jenis_barang`) VALUES
 (1, 'Jenis A'),
-(3, 'Jenis B'),
-(4, 'Jenis C');
+(2, 'komputer'),
+(3, 'asesoris'),
+(4, 'hardware'),
+(5, 'uuy'),
+(6, 'Jenis C');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laporan`
+--
+
+CREATE TABLE `laporan` (
+  `id` int(11) NOT NULL,
+  `nama_barang` varchar(50) DEFAULT NULL,
+  `kode_barang` varchar(50) DEFAULT NULL,
+  `harga_beli` int(50) DEFAULT NULL,
+  `harga_jual` int(11) NOT NULL,
+  `stok_awal` int(12) DEFAULT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `jumlah_jual` int(11) NOT NULL,
+  `jumlah_akhir` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `laporan`
+--
+
+INSERT INTO `laporan` (`id`, `nama_barang`, `kode_barang`, `harga_beli`, `harga_jual`, `stok_awal`, `jumlah_beli`, `jumlah_jual`, `jumlah_akhir`) VALUES
+(2, 'Mesin', 'BRG0007', 212, 211, NULL, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -154,7 +183,9 @@ CREATE TABLE `merk_barang` (
 
 INSERT INTO `merk_barang` (`id_merk`, `merk_barang`) VALUES
 (1, 'Merk Super'),
-(2, 'MERK KW');
+(2, 'Huawei'),
+(3, 'aaad'),
+(4, 'MERK KW');
 
 -- --------------------------------------------------------
 
@@ -175,8 +206,8 @@ CREATE TABLE `supplier` (
 --
 
 INSERT INTO `supplier` (`id_supplier`, `kode_supplier`, `nama_supplier`, `username`, `password`) VALUES
-(1, 'sp001', 'Supplier A', 'suppliera', 'suppliera'),
-(2, 'sp002', 'Supplier B', 'supplierb', 'supplierb');
+(1, 'suppliera', 'suppliera', 'suppliera', 'suppliera'),
+(2, 'supplierb', 'supplierb', 'supplierb', 'supplierb');
 
 -- --------------------------------------------------------
 
@@ -201,6 +232,21 @@ INSERT INTO `transaksi` (`id_transaksi`, `kode_transaksi`, `tgl_transaksi`, `tot
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `transaksi_penjualan`
+--
+
+CREATE TABLE `transaksi_penjualan` (
+  `id_transaksi` int(11) NOT NULL,
+  `kode_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(20) NOT NULL,
+  `jumlah_item` int(50) NOT NULL,
+  `harga` int(20) NOT NULL,
+  `tanggal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -218,8 +264,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id_user`, `nama_user`, `username`, `password`, `level`) VALUES
 (1, 'Administrator', 'admin', 'admin', 'admin'),
-(2, 'Petugas Gudang', 'gudang', 'gudang', 'petugas gudang'),
-(3, 'Manajer', 'manajer', 'manajer', 'manajer');
+(2, 'Petugas Gudang', 'gudang', 'gudang', 'petugas gudang');
 
 --
 -- Indexes for dumped tables
@@ -229,25 +274,46 @@ INSERT INTO `users` (`id_user`, `nama_user`, `username`, `password`, `level`) VA
 -- Indexes for table `barang`
 --
 ALTER TABLE `barang`
-  ADD PRIMARY KEY (`id_barang`);
+  ADD PRIMARY KEY (`id_barang`),
+  ADD UNIQUE KEY `kode_barang` (`kode_barang`),
+  ADD UNIQUE KEY `nama_barang_2` (`nama_barang`),
+  ADD KEY `nama_barang` (`nama_barang`),
+  ADD KEY `stok` (`stok`);
 
 --
 -- Indexes for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
-  ADD PRIMARY KEY (`id_barang_keluar`);
+  ADD PRIMARY KEY (`id_barang_keluar`),
+  ADD KEY `harga` (`harga_jual`),
+  ADD KEY `jumlah` (`jumlah_jual`);
 
 --
 -- Indexes for table `barang_masuk`
 --
 ALTER TABLE `barang_masuk`
-  ADD PRIMARY KEY (`id_barang_masuk`);
+  ADD PRIMARY KEY (`id_barang_masuk`),
+  ADD UNIQUE KEY `harga` (`harga_beli`),
+  ADD KEY `jumlah` (`jumlah_beli`);
 
 --
 -- Indexes for table `jenis_barang`
 --
 ALTER TABLE `jenis_barang`
   ADD PRIMARY KEY (`id_jenis`);
+
+--
+-- Indexes for table `laporan`
+--
+ALTER TABLE `laporan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nama_barang` (`nama_barang`),
+  ADD KEY `kode_barang` (`kode_barang`),
+  ADD KEY `harga_beli` (`harga_beli`),
+  ADD KEY `harga_jual` (`harga_jual`),
+  ADD KEY `stok_awal` (`stok_awal`),
+  ADD KEY `jumlah_beli` (`jumlah_beli`),
+  ADD KEY `jumlah_jual` (`jumlah_jual`);
 
 --
 -- Indexes for table `merk_barang`
@@ -268,6 +334,12 @@ ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`);
 
 --
+-- Indexes for table `transaksi_penjualan`
+--
+ALTER TABLE `transaksi_penjualan`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -281,12 +353,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
-  MODIFY `id_barang_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_barang_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `barang_masuk`
 --
@@ -296,12 +368,17 @@ ALTER TABLE `barang_masuk`
 -- AUTO_INCREMENT for table `jenis_barang`
 --
 ALTER TABLE `jenis_barang`
-  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `laporan`
+--
+ALTER TABLE `laporan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `merk_barang`
 --
 ALTER TABLE `merk_barang`
-  MODIFY `id_merk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_merk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `supplier`
 --
@@ -313,10 +390,15 @@ ALTER TABLE `supplier`
 ALTER TABLE `transaksi`
   MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT for table `transaksi_penjualan`
+--
+ALTER TABLE `transaksi_penjualan`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
